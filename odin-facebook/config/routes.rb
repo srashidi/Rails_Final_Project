f684bcdb48861f4458b3e9a29b68479d1a72d705
Shaunty Rashidi <shauntyrashidi@shauntycomputer.sdcs.local>
2016-03-17 12:51:22 -0700

    Implement devise gem for User model

--- odin-facebook/config/routes.rb
+++ odin-facebook/config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
+  get 'friendships/show'
+
+  get 'posts/index'
+
   get 'users/new'
-
   get 'users/show'
-
   get 'users/index'
-
   get 'users/edit'
+  devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
